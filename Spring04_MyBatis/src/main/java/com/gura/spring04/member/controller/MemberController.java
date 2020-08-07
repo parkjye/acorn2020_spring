b405fa71e47799194fb06be53b578beb0a3f9d43
parkjye <parkjye44@gmail.com>
2020-08-07 12:25:00 +0900

    <Feat> DB연동 예제 추가합니다.

--- Spring04_MyBatis/src/main/java/com/gura/spring04/member/controller/MemberController.java
+++ Spring04_MyBatis/src/main/java/com/gura/spring04/member/controller/MemberController.java
@@ -1,18 +1,34 @@
 package com.gura.spring04.member.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.gura.spring04.member.dao.MemberDao;
 import com.gura.spring04.member.dto.MemberDto;
 
 @Controller
 public class MemberController {
+	
+	//의존객체를 주입 받는다.
+	@Autowired
+	private MemberDao dao;
+	
+	@RequestMapping("/member/list")
+	public ModelAndView getList(ModelAndView mView) {
+		//회원목록을 얻어온다.
+		List<MemberDto> list = dao.getList();
 		
-	//회원 정보 추가 요청 처리
-	@RequestMapping("/member/insert")
-	public ModelAndView insert(MemberDto dto) {
+		//model을 담는다.
+		mView.addObject("list", list);
 		
-		return null;
+		//view 페이지 정보를 담고(forward이동)
+		mView.setViewName("member/list");
+		
+		//ModelAndView 객체를 리턴해준다.
+		return mView;
 	}
 }
