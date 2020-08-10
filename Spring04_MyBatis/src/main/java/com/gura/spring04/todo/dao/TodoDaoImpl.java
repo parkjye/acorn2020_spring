28c8aadbb9549747385105f42f0b23fbb45d66ef
parkjye <parkjye44@gmail.com>
2020-08-10 18:10:13 +0900

    <Fix> To to list implements 수정

--- Spring04_MyBatis/src/main/java/com/gura/spring04/todo/dao/TodoDaoImpl.java
+++ Spring04_MyBatis/src/main/java/com/gura/spring04/todo/dao/TodoDaoImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
 import com.gura.spring04.todo.dto.TodoDto;
 
--- Spring04_MyBatis/src/main/java/com/gura/spring04/todo/service/TodoServiceImpl.java
+++ Spring04_MyBatis/src/main/java/com/gura/spring04/todo/service/TodoServiceImpl.java
@@ -3,6 +3,7 @@ package com.gura.spring04.todo.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.ModelAndView;
 
