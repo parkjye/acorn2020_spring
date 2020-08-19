4596c2b2f0f002fabdc17c6f0bc6a6501cd8f969
parkjye <parkjye44@gmail.com>
2020-08-19 16:07:22 +0900

    <Fix> cafe :: 기본 구성 완료 오탈자 수정

--- Spring05_Final/src/main/java/com/gura/spring05/cafe/service/CafeService.java
+++ Spring05_Final/src/main/java/com/gura/spring05/cafe/service/CafeService.java
@@ -1,8 +1,5 @@
 package com.gura.spring05.cafe.service;
 
-import org.springframework.stereotype.Service;
-
-@Service
 public interface CafeService {
-
+	
 }
--- Spring05_Final/src/main/java/com/gura/spring05/cafe/service/CafeServiceImpl.java
+++ Spring05_Final/src/main/java/com/gura/spring05/cafe/service/CafeServiceImpl.java
@@ -1,11 +1,12 @@
 package com.gura.spring05.cafe.service;
 
-import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.gura.spring05.cafe.dao.CafeDao;
 
 @Service
 public class CafeServiceImpl implements CafeService{
 	@Autowired
-	private SqlSession session;
+	private CafeDao cafeDao;
 }
