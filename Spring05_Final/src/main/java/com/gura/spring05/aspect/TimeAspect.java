8ad8912ae3eaa41b037ccaca3897c23e9b939471
parkjye <parkjye44@gmail.com>
2020-08-24 15:41:05 +0900

    <Fix> AOP :: log4j 오류수정

--- Spring05_Final/src/main/java/com/gura/spring05/aspect/TimeAspect.java
+++ Spring05_Final/src/main/java/com/gura/spring05/aspect/TimeAspect.java
@@ -15,20 +15,23 @@ public class TimeAspect {
 	public Object checkTime(ProceedingJoinPoint joinPoint) throws Throwable {
 		
 		//로그를 출력할 객체의 참조값 얻어오기
-		Logger logger = LoggerFactory.getLogger(joinPoint.getClass());
+		Logger logger = LoggerFactory.getLogger(joinPoint.getTarget().getClass());
+		logger.info("요청 처리 시작!");
 		
-		//logger.info("요청처리 시작");
-		System.out.println("요청처리 시작");
+		//시작시간(ms)
 		long startTime = System.currentTimeMillis();
 		
 		//AOP가 적용된 메소드 호출하기
 		Object obj = joinPoint.proceed();
 		
+		//끝시간(ms)
 		long endTime = System.currentTimeMillis();
+		
+		//시작과 끝의 차이를 얻어낸다.
 		long term = startTime-endTime;
 		System.out.println("요청처리에 소요된 시간: "+term);
-		System.out.println("요청처리 끝");
-		//logger.info("요청처리 끝");
+		logger.info("요청 처리 끝!");
+
 		return obj;
 	}
 }
