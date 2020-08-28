40b70222ede68d32c282ac90400ab16705b286c5
parkjye <parkjye44@gmail.com>
2020-08-28 16:24:16 +0900

    <Feat> angularjs :: cafe글 목록 불러오기

--- Spring05_Final/src/main/java/com/gura/spring05/cafe/service/CafeService.java
+++ Spring05_Final/src/main/java/com/gura/spring05/cafe/service/CafeService.java
@@ -1,5 +1,7 @@
 package com.gura.spring05.cafe.service;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
 import com.gura.spring05.cafe.dto.CafeCommentDto;
@@ -17,4 +19,7 @@ public interface CafeService {
 	public void deleteComment(HttpServletRequest request); //삭제
 	public void updateComment(CafeCommentDto dto); //수정
 	public void moreCommentList(HttpServletRequest request);//댓글 추가 응답
+	
+	//ajax
+	public List<CafeDto> getList2(HttpServletRequest request);
 }
